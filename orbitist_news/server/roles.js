b997de2c6a29a6397f5a7c70f94f756a03575be1
Nicholas Gunner <ngunner@Nicholass-MacBook-Pro.local>
2015-10-25 13:45:49 -0400

    many updates

--- orbitist_news/server/roles.js
+++ orbitist_news/server/roles.js
@@ -1,5 +1,9 @@
 Meteor.startup(function () {
 	// bootstrap the admin user if they exist -- You'll be replacing the id later
-	if (Meteor.users.findOne("LRRCHrmBT3qPHuzbW"))
-		Roles.addUsersToRoles("LRRCHrmBT3qPHuzbW", ['admin']);
+	if (Meteor.users.findOne("tAQmnyhTj9D9aLjqp"))
+		Roles.addUsersToRoles("tAQmnyhTj9D9aLjqp", ['admin']);
+		
+	// on meteor.com	
+	if (Meteor.users.findOne("feXiQj26E5PvXXAGZ"))
+		Roles.addUsersToRoles("feXiQj26E5PvXXAGZ", ['admin']);
 });
