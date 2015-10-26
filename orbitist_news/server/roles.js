e8db34f0ab0c781a7e7a68d1919ddcee9678eedd
Nicholas Gunner <ngunner@Nicholass-MacBook-Pro.local>
2015-10-26 04:55:18 -0400

    Made map story slider stay out by default

--- orbitist_news/server/roles.js
+++ orbitist_news/server/roles.js
@@ -1,9 +1,13 @@
 Meteor.startup(function () {
-	// bootstrap the admin user if they exist -- You'll be replacing the id later
+	// admin users on nick's local
 	if (Meteor.users.findOne("tAQmnyhTj9D9aLjqp"))
 		Roles.addUsersToRoles("tAQmnyhTj9D9aLjqp", ['admin']);
 		
 	// on meteor.com	
 	if (Meteor.users.findOne("feXiQj26E5PvXXAGZ"))
 		Roles.addUsersToRoles("feXiQj26E5PvXXAGZ", ['admin']);
+		
+	// on production
+	if (Meteor.users.findOne("Q7K7pkA5E3NPGQo5v"))
+		Roles.addUsersToRoles("Q7K7pkA5E3NPGQo5v", ['admin']);
 });
